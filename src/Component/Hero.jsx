0fc9f77742a45473724d5c55aea4f426932996a3
Messiovo <messiovo@gmail.com>
2022-07-08 15:51:25 +0100

    changes to gero

--- src/Component/Hero.jsx
+++ src/Component/Hero.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React from 'react';
+import hero1 from './image/hero1.jpg';
+import hero2 from './image/hero2.jpg';
 
 const Hero = () => {
   return (
-    <div>Hero world</div>
+    <div>
+      <div className='bg-[#05185B] flex flex-col lg:flex-row'>
+        <div className='text-white'>
+          <img src={hero1} alt="" className='w-64'/>
+          <h2>Kids Coding</h2>
+          <p>help your kids discovery the tech in them</p>
+        </div>
+        <div className='text-white'>
+          <img src={hero2} alt="" className='w-64' />
+          <h2>Abacus Mental Math</h2>
+          <p>Create genius in mathematics with speed and accuracy</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
