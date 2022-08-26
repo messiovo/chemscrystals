e7912c414d86a99484518b6341425e3cf44cafb2
Messiovo <messiovo@gmail.com>
2022-08-26 19:30:10 +0100

    reduce padding of request button

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -179,7 +179,7 @@ const Home = () => {
           It will only take a minute
         </span>
         <Link to="/">
-          <button className="rounded-full px-16 py-2 mt-10 bg-[#495371] font-bold m-auto block text-[#FDDB93] text-sm lg:text-md hover:scale-110 transition-all duration-300 ease-in ">
+          <button className="rounded-full px-[3rem] py-2 mt-10 bg-[#495371] font-bold m-auto block text-[#FDDB93] text-sm lg:text-md hover:scale-110 transition-all duration-300 ease-in ">
             Request a demo class
           </button>
         </Link>
