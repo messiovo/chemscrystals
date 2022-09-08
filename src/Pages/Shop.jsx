5cfcaa18c3136604c0e54392c85c76a3c644bdc3
Messiovo <messiovo@gmail.com>
2022-09-08 21:43:01 +0100

    changes to shopping page

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -3,16 +3,15 @@ import React from "react";
 const Shop = () => {
   return (
     <>
-      <section className=" p-10 mt-10 bg-[url(./styles/image/shopping.jpg)] flex flex-col items-center justify-center  bg-dark/90 bg-no-repeat bg-cover bg-center bg-blend-saturation ">
-        <h1 className=" text-lg lg:text-xl font-bold pb-4 text-light text-center">
+      <section className=" px-10 py-20 mt-16 bg-[url(./image/secttion3.png)] flex flex-col items-center justify-center  bg-dark/90 bg-no-repeat bg-cover bg-center bg-blend-saturation text-light">
+        <h1 className="font-bold text-xl lg:text-3xl md:text-2xl pb-4 text-center">
           Let us bring your educative materials to your doorstep
         </h1>
-        <p className="text-sm lg:text-lg leading-8  px-4 text-light text-center">
+        <p className="font-semi-bold  lg:text-lg md:text-lg pb-4 text-md">
           educative games, teaching aids, back to shool items and lots more
         </p>
-
         <button
-          className="rounded-full px-16 py-2 mt-10 mb-6 bg-dark font-bold m-auto block text-light  text-sm lg:text-md hover:scale-110 transition-all duration-300 ease-in "
+          className="rounded-full text-dark px-16 py-2 bg-medium font-bold m-auto block hover:scale-110 transition-all duration-300 ease-in text-sm lg:text-md"
           data-aos="fade-up"
         >
           Shop now
