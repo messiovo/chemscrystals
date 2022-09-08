86b89f2a80e5c335d46d6658c7d554073ac10c4e
Messiovo <messiovo@gmail.com>
2022-09-08 13:28:10 +0100

    adding svgs and changing nav

--- src/Component/SectionB.jsx
+++ src/Component/SectionB.jsx
@@ -9,16 +9,14 @@ const SectionB = () => {
           Student Learning Platform
         </h1>
         <p className="text-sm lg:text-lg leading-8  px-4 text-light text-center">
-          ed ut perspiciatis unde omnis iste natus error sit voluptatem
-          accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae
-          ab illo inventore veritatis et quasi architecto beatae.totam.
+          Wherever you are, start learning and improve your skills on our various courses. 
         </p>
         <Link to="/">
           <button
             className="rounded-full px-16 py-2 mt-10 mb-6 bg-dark font-bold m-auto block text-light  text-sm lg:text-md hover:scale-110 transition-all duration-300 ease-in "
             data-aos="fade-up"
           >
-            Get App
+            Explore
           </button>
         </Link>
       </section>
